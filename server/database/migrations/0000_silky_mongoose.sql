3680a3236d02af2e7d9ed7818745e84602c4ce7d
Victor Otávio Ferreira <victor@ferreira.id>
2024-04-11 14:21:49 -0300

    refact: update table `songs` schema

--- server/database/migrations/0000_silky_mongoose.sql
+++ server/database/migrations/0000_silky_mongoose.sql
@@ -1,9 +1,12 @@
 CREATE TABLE `songs` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`title` text(100) NOT NULL,
+	`artist` text(50) NOT NULL,
 	`lyrics` text NOT NULL,
 	`chords` text NOT NULL,
 	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	`updated_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	`deleted_at` text
 );
+--> statement-breakpoint
+CREATE UNIQUE INDEX `songs_title_artist_unique` ON `songs` (`title`,`artist`);
