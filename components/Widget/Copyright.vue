8716c06cb0f69a7a676e3f42e95c422d288d4ca1
Victor Otávio Ferreira <victor@ferreira.id>
2024-04-18 14:07:55 -0300

    fix: copyright component

--- components/Widget/Copyright.vue
+++ components/Widget/Copyright.vue
@@ -1,11 +1,13 @@
 <template>
-  <h2
-    class="mt-10 flex items-center font-mono text-sm leading-7 text-cool-900 lg:mt-12 dark:text-cool-100"
-  >
-    <IconsTitle /><span class="ml-2.5">Idealizado e desenvolvido por</span>
-  </h2>
-  <div class="mt-2 flex gap-6 text-sm font-bold leading-7 text-cool-700 dark:text-cool-300">
-    <NuxtLink href="http://twitter.com/vctrtvfrrr" target="_blank">Victor Ferreira</NuxtLink
-    ><!--<span aria-hidden="true" class="text-cool-400 dark:text-cool-600">/</span>mais alguém?-->
-  </div>
+  <section>
+    <h2
+      class="mt-10 flex items-center font-mono text-sm leading-7 text-cool-900 lg:mt-12 dark:text-cool-100"
+    >
+      <IconsTitle /><span class="ml-2.5">Idealizado e desenvolvido por</span>
+    </h2>
+    <div class="mt-2 flex gap-6 text-sm font-bold leading-7 text-cool-700 dark:text-cool-300">
+      <NuxtLink href="http://twitter.com/vctrtvfrrr" target="_blank">Victor Ferreira</NuxtLink
+      ><!--<span aria-hidden="true" class="text-cool-400 dark:text-cool-600">/</span>mais alguém?-->
+    </div>
+  </section>
 </template>
