b2d2e87b0b7bab380d2118c5056996748acb5fda
Victor Otávio Ferreira <victor@ferreira.id>
2024-05-03 17:57:16 -0300

    fix: resolves Node Experimental Warning

--- register.js
+++
@@ -1,6 +0,0 @@
-// Needed to remove the '--experimental-loader=module' warning:
-// https://nodejs.org/api/cli.html#--experimental-loadermodule
-import { register } from 'node:module'
-import { pathToFileURL } from 'node:url'
-
-register('ts-node/esm', pathToFileURL('./'))
